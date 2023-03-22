2aebe7dca22955aa5005d432e55a502d12cb5ce8
Yane van der Westhuizen <93937046+yanevdw19021209@users.noreply.github.com>
2023-03-22 22:29:17 +0200

    Updated HTML and CSS for home page

--- libs/app/home/feature/src/home.page.ts
+++ libs/app/home/feature/src/home.page.ts
@@ -4,6 +4,7 @@ import { ProfileState } from '@mp/app/profile/data-access';
 import { SubscribeToProfile } from '@mp/app/profile/util';
 import { Select, Store } from '@ngxs/store';
 import { Observable } from 'rxjs';
+import {profile} from './profile.interface';
 
 @Component({
   selector: 'ms-home-page',
@@ -12,10 +13,21 @@ import { Observable } from 'rxjs';
 })
 export class HomePage {
   @Select(ProfileState.profile) profile$!: Observable<IProfile | null>;
+  array_profile !: profile [];
 
-  constructor(private readonly store: Store) {}
+ 
 
-  ionViewWillEnter() {
+  deleteItem()
+  {
+
+  }
+
+  constructor(private readonly store: Store) {
+    this.array_profile = [{name : "Emily", age : 23, interests: ["At Uni", "Aries", "Vegan"]}, {name : "Emily", age : 23, interests: ["At Uni", "Aries", "Vegan"]}];
+  }
+
+  ionViewWillEnter() 
+  {
     this.store.dispatch(new SubscribeToProfile());
   }
 }
