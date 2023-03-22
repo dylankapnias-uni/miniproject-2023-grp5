d0393b76ccc4e5b2322680440581f3d6a65f1857
AmickeC <croucampa01@gmail.com>
2023-03-22 16:36:24 +0200

    profile UI

--- libs/app/profile/feature/src/profile.page.ts
+++ libs/app/profile/feature/src/profile.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild } from '@angular/core';
 import { IProfile } from '@mp/api/profiles/util';
 import { ProfileState } from '@mp/app/profile/data-access';
 import { Select } from '@ngxs/store';
@@ -11,4 +11,17 @@ import { Observable } from 'rxjs';
 })
 export class ProfilePage {
   @Select(ProfileState.profile) profile$!: Observable<IProfile | null>;
+  
+  
+
+  slideOpts = {
+    initialSlide: 0,
+    speed: 400
+  };
+
+  images = [
+    'https://example.com/image1.jpg',
+    'https://example.com/image2.jpg',
+    'https://example.com/image3.jpg'
+  ];
 }
