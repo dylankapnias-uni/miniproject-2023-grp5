6fcf8620b5ee5492b8cbae312c49847d5769974a
Thomas Johan Blendulf <u21446131@tuks.co.za>
2023-04-15 01:40:51 +0200

    Created Profile API

--- libs/api/user-profile/data-access/src/user-profile.repository.ts
+++ libs/api/user-profile/data-access/src/user-profile.repository.ts
@@ -1,4 +1,39 @@
+import { IProfile } from '@mp/api/user-profiles/util';
 import { Injectable } from '@nestjs/common';
 import * as admin from 'firebase-admin';
 @Injectable()
-export class ProfileRepository {}
+export class ProfileRepository {
+    async getUserByID(profile: IProfile) {
+        return await admin
+          .firestore()
+          .collection('User_Profile')
+          .withConverter<IProfile>({
+            fromFirestore: (snapshot) => {
+              return snapshot.data() as IProfile;
+            },
+            toFirestore: (it: IProfile) => it,
+          })
+          .doc(profile.userId)
+          .get();
+      }
+    
+      async createProfile(profile: IProfile) {
+        // Remove password field if present
+        delete profile.accountDetails?.password;
+        return await admin
+          .firestore()
+          .collection('User_Profile')
+          .doc(profile.userId)
+          .create(profile);
+      }
+    
+      async updateProfile(profile: IProfile) {
+        // Remove password field if present
+        delete profile.accountDetails?.password;
+        return await admin
+          .firestore()
+          .collection('User_Profile')
+          .doc(profile.userId)
+          .set(profile, { merge: true });
+      }
+}
