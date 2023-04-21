3678f25da312a9dacdd32b587a49c847f27750d5
Wian Koekemoer <105972926+slaaiblaar@users.noreply.github.com>
2023-04-22 00:51:29 +0200

    Added UserProfileService methods and https functions in core

--- libs/api/users/feature/src/user-profile.module.ts
+++ libs/api/users/feature/src/user-profile.module.ts
@@ -1,16 +1,36 @@
 import { UserProfileModule as UserProfileDataAccessModule } from '@mp/api/users/data-access';
 import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
-import { CreateUserHandler } from './commands';
-import { UserCreatedHandler } from './events';
+import { CreateUserHandler, UpdateUserProfileHandler, DeleteUserProfileHandler } from './commands';
+import { UserCreatedHandler, UserProfileUpdatedHandler, UserProfileDeletedHandler } from './events';
+import { GetUserProfileHandler } from './queries';
 import { UsersSagas } from './user-profile.sagas';
-import { UsersService } from './user-profile.service';
-export const CommandHandlers = [CreateUserHandler];
-export const EventHandlers = [UserCreatedHandler];
+import { UserProfileService } from './user-profile.service';
 
+export const CommandHandlers = [
+  CreateUserHandler,
+  UpdateUserProfileHandler,
+  DeleteUserProfileHandler
+];
+
+export const EventHandlers = [
+  UserCreatedHandler,
+  UserProfileUpdatedHandler,
+  UserProfileDeletedHandler
+];
+
+export const QueryHandlers = [
+  GetUserProfileHandler
+];
 @Module({
   imports: [CqrsModule, UserProfileDataAccessModule],
-  providers: [UsersService, ...CommandHandlers, ...EventHandlers, UsersSagas],
-  exports: [UsersService],
+  providers: [
+    UserProfileService, 
+    ...CommandHandlers, 
+    ...EventHandlers, 
+    ...QueryHandlers, 
+    UsersSagas
+  ],
+  exports: [UserProfileService],
 })
 export class UsersModule {}
--- libs/api/users/feature/src/user-profile.service.ts
+++ libs/api/users/feature/src/user-profile.service.ts
@@ -1,4 +1,52 @@
 import { Injectable } from '@nestjs/common';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
+import { 
+  IGetUserProfileRequest, 
+  IUpdateUserProfileRequest,
+  IDeleteUserProfileRequest 
+} from '../../util/src/requests';
+import { 
+  IGetUserProfileResponse, 
+  IUpdateUserProfileResponse,
+  IDeleteUserProfileResponse 
+} from '../../util/src/responses';
+import { GetUserProfileQuery } from '../../util/src/queries';
+import { 
+  UpdateUserProfileCommand, 
+  DeleteUserProfileCommand 
+} from '@mp/api/users/util';
 
 @Injectable()
-export class UsersService {}
+export class UserProfileService {
+  constructor(
+      private readonly commandBus: CommandBus,
+      private readonly queryBus: QueryBus
+    ) {}
+
+    async getUserProfile(
+      request: IGetUserProfileRequest
+    ): Promise<IGetUserProfileResponse> {
+      return await this.queryBus.execute<
+        GetUserProfileQuery,
+        IGetUserProfileResponse
+      >(new GetUserProfileQuery(request));
+    }
+
+    async updateUserProfile(
+      request: IUpdateUserProfileRequest
+    ): Promise<IUpdateUserProfileResponse> {
+      return await this.commandBus.execute<
+        UpdateUserProfileCommand,
+        IUpdateUserProfileResponse
+      >(new UpdateUserProfileCommand(request));
+    }
+
+    async deleteUserProfile(
+      request: IDeleteUserProfileRequest
+    ): Promise<IDeleteUserProfileResponse> {
+      return await this.commandBus.execute<
+        DeleteUserProfileCommand,
+        IDeleteUserProfileResponse
+      >(new DeleteUserProfileCommand(request));
+    }
+}
