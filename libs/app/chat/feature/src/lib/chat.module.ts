5dbcd8bc3bff38468761571c7387d0c18f417614
Adrian-Hawkins <u21669831@tuks.co.za>
2023-04-21 15:34:35 +0200

    Added countdown feature to chat as well chat state

--- libs/app/chat/feature/src/lib/chat.module.ts
+++ libs/app/chat/feature/src/lib/chat.module.ts
@@ -4,9 +4,11 @@ import { ChatPage } from './chat.page';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { ChatRouting } from './chat.routing';
+import { NgxsModule } from '@ngxs/store';
+import { ChatState } from '@mp/app/chat/data-access';
 
 @NgModule({
-  imports: [CommonModule, IonicModule, FormsModule, ChatRouting],
+  imports: [CommonModule, IonicModule, FormsModule, ChatRouting , NgxsModule.forFeature([ChatState])],
   declarations: [ChatPage],
 })
 export class ChatModule {}
