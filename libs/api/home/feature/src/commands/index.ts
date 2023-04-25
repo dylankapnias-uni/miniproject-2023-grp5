46ed3ce061564690f0ff6c21039035494cb3b68a
Thomas Johan Blendulf <u21446131@tuks.co.za>
2023-04-25 11:33:12 +0200

    Fixed Imports

--- libs/api/home/feature/src/commands/index.ts
+++ libs/api/home/feature/src/commands/index.ts
@@ -1,3 +1,3 @@
 export * from './accept-user.handler';
 export * from './reject-user.handler';
-export * from './create-home-user.handler';
+export * from './create-user-home.handler';
--- libs/api/home/util/src/events/index.ts
+++ libs/api/home/util/src/events/index.ts
@@ -1,4 +1,4 @@
-export * from './home-created.event';
+export * from './user-home-created.event';
 export * from './user-accepted.event';
 export * from './user-rejected.event';
 export * from './user-home-created.event';
