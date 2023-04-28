72157acaa5a47b2cb9b5ddd2a5bc3e38a8f3580a
Enchantress1416 <u21804312@tuks.co.za>
2023-04-29 00:18:54 +0200

    Added missing dependancy

--- libs/api/chat/feature/jest.config.ts
+++ libs/api/chat/feature/jest.config.ts
@@ -11,5 +11,5 @@ export default {
     '^.+\\.[tj]s$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../../../coverage/libs/api/chat/feature',
+  coverageDirectory: '../../../../coverage/libs/api/chat/',
 };
--- libs/api/chat/feature/src/api-chat-feature.spec.ts
+++ libs/api/chat/feature/src/api-chat-feature.spec.ts
@@ -1,20 +1,25 @@
-import { CommandBus } from "@nestjs/cqrs";
+import { CommandBus, QueryBus } from "@nestjs/cqrs";
 import { ChatService } from "./chat.service";
 import { Test, TestingModule } from '@nestjs/testing';
+import { ICreateChatRequest, ICreateChatResponse, IChat, CreateChatQuery, IGetChatRequest } from "@mp/api/chat/util";
 
 
 describe("Chat", () => {
     let commandBus: CommandBus
+    let queryBus: QueryBus
     let chatService: ChatService
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            providers: [ChatService, CommandBus],
+            providers: [ChatService, CommandBus, QueryBus],
           }).compile();
       
           chatService = module.get<ChatService>(ChatService);
           commandBus = module.get<CommandBus>(CommandBus);
-    })
+          queryBus = module.get<QueryBus>(QueryBus)
+    }),
+
+    
 
 
 });
