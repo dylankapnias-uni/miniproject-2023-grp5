b8ff1c8cf225eee80405f918ed3de3cb53f30968
Enchantress1416 <u21804312@tuks.co.za>
2023-04-28 23:36:58 +0200

    Implemented fetch user with valid user and not matched

--- libs/api/home/feature/src/api-home-feature.spec.ts
+++ libs/api/home/feature/src/api-home-feature.spec.ts
@@ -198,6 +198,42 @@ describe("API Home Feature Tests", () => {
         expect(commandBus.execute).toHaveBeenCalledWith(new RetrieveHomeUsersQuery(mockRequest))
         expect(result).toBe(mockResponse)
         expect(result.users.userList[0].match).toBe(true)
+      }),
+
+      it("Fetch valid user with filter and not matched", async () => {
+        //given
+        const mockFilter: IInterests = {
+          interest: "mock",
+          category: "fake"
+        }
+        const mockRequest: IRetrieveHomeUsersRequest = {
+          userId: "mockUser",
+          filter: [mockFilter]
+        }
+        const mockUserProfile: IUserProfile = {
+          userId: "anotherUser"
+        }
+        const mockUserMatch: IUserMatch = {
+          user: mockUserProfile,
+          match: false
+        }
+        const mockUsers: IHome = {
+          userId: "mockUser",
+          userList: [mockUserMatch]
+        }
+        const mockResponse: IRetrieveHomeUsersResponse = {
+          users: mockUsers
+        }
+
+        jest.spyOn(commandBus, "execute").mockResolvedValueOnce(mockResponse)
+
+        //when
+        const result = await homeService.fetchUsers(mockRequest)
+
+        //then
+        expect(commandBus.execute).toHaveBeenCalledWith(new RetrieveHomeUsersQuery(mockRequest))
+        expect(result).toBe(mockResponse)
+        expect(result.users.userList[0].match).toBe(false)
       })
 
     })
