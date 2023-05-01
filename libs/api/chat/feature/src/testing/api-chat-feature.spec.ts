9acc7241ac3b370e02f90462e7f82376987eeb08
Enchantress1416 <u21804312@tuks.co.za>
2023-05-01 17:30:44 +0200

    Removed redundant test

--- libs/api/chat/feature/src/testing/api-chat-feature.spec.ts
+++
@@ -1,7 +0,0 @@
-import { apiChatFeature } from './api-chat-feature';
-
-describe('apiChatFeature', () => {
-  it('should work', () => {
-    expect(apiChatFeature()).toEqual('api-chat-feature');
-  });
-});
--- libs/api/chat/feature/src/testing/api-chat-feature.ts
+++
@@ -1,3 +0,0 @@
-export function apiChatFeature(): string {
-  return 'api-chat-feature';
-}
