7fa3cd15456947dd01b766d3554c11f6f54be7ff
Enchantress1416 <u21804312@tuks.co.za>
2023-05-01 17:39:14 +0200

    Removed redundant test

--- libs/api/notifications/feature/src/testing/api-notifications-feature.spec.ts
+++
@@ -1,7 +0,0 @@
-import { apiNotificationsFeature } from './api-notifications-feature';
-
-describe('apiNotificationsFeature', () => {
-  it('should work', () => {
-    expect(apiNotificationsFeature()).toEqual('api-notifications-feature');
-  });
-});
