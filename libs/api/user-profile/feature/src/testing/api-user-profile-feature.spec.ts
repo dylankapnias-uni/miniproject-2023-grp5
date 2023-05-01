6986b654626d49186b97b8367f44e355f94913e0
Enchantress1416 <u21804312@tuks.co.za>
2023-05-01 17:43:19 +0200

    Removed redundant test

--- libs/api/user-profile/feature/src/testing/api-user-profile-feature.spec.ts
+++
@@ -1,7 +0,0 @@
-import { apiUserProfileFeature } from './api-user-profile-feature';
-
-describe('apiUserProfileFeature', () => {
-  it('should work', () => {
-    expect(apiUserProfileFeature()).toEqual('api-user-profile-feature');
-  });
-});
