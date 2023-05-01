c76c9114cb13cd096656cddc4932da03da2c6786
Enchantress1416 <u21804312@tuks.co.za>
2023-05-01 17:42:55 +0200

    Removed redundant test

--- libs/api/user-profile/data-access/src/testing/api-user-profile-data-access.spec.ts
+++
@@ -1,7 +0,0 @@
-import { apiUserProfileDataAccess } from './api-user-profile-data-access';
-
-describe('apiUserProfileDataAccess', () => {
-  it('should work', () => {
-    expect(apiUserProfileDataAccess()).toEqual('api-user-profile-data-access');
-  });
-});
